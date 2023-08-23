8ea9fdebe4640ed5ee0b12494a1976e0d0ed743b
Maxim Efimov <maksimyss2000@mail.ru>
2023-08-23 03:31:02 +0300

    work version

--- client/client.c
+++ client/client.c
@@ -15,21 +15,52 @@
 #define START_JSON "{\"data\":\""
 #define END_JSON  "\"}"
 
-typedef struct Buffer {
-    int max_size;
-    int current_size;
-    char* message;
-} Buffer; 
+char* readMessageBySoket(int client_sd) {
+    int size_buffer = START_SIZE_BUFFER;
+    int reading_size = size_buffer;
+    char* message = (char*)calloc(size_buffer + 1, sizeof(char));
+    char* ptr = message;
+    int count_read = 0;
+    int index = 0;
+    while ((count_read = read(client_sd, ptr, reading_size)) == reading_size) {
+        size_buffer += reading_size;
+        message = (char*)realloc(message, size_buffer + 1);
+        index += count_read;
+        ptr = message + index;
+    }
+    index += count_read;
+    message[index] = '\0';
+    return message;
+}
 
-printHash(unsigned char* sha512, unsigned char* gost) {
-    for (int i = 0; i < mhash_get_block_size(MHASH_SHA512); i++) {
-		printf("%.2x", sha512[i]);
-	}
-	printf("\n");
-    	for (int i = 0; i < mhash_get_block_size(MHASH_GOST); i++) {
-		printf("%.2x", gost[i]);
-	}
-	printf("\n");
+int parsePesponse(char* request, char** hash_sha512, char** hash_gost) {
+    char* field_sha512 = strstr(request, "\"sha512\"");
+    char* field_gost = strstr(request, "\"gost\"");
+    char* method = strtok(request, " ");
+    char* status = strtok(NULL, " ");
+    char* version = strtok(NULL, " \n");
+    if (strcmp(status, "200")) {
+        return atoi(status);
+    }
+
+    strtok(NULL, " ");                     /*skip "Accept:"*/
+    char* type_accept = strtok(NULL, " \n");
+
+    *hash_sha512 = strtok(field_sha512 + strlen("\"sha512\""), ":\"");
+    *hash_gost  = strtok(field_gost + strlen("\"gost\""), ":\"");
+    return atoi(status);
+}
+
+void hanleRespone(char* response_message) {
+    char* hash_sha512;
+    char* hash_gost;
+    int code = parsePesponse(response_message, &hash_sha512, &hash_gost);
+    if (code == 200) {
+        printf("hash_sha512(data) = %s\n", hash_sha512);
+        printf("hash_gost(data) = %s\n", hash_gost);
+    } else {
+        printf("response error %d\n", code);
+    }
 }
 
 int createSoket() {
@@ -56,31 +87,38 @@ void setAddresSetting(struct sockaddr_in* serv_addr) {
     serv_addr->sin_addr.s_addr = inet_addr(ADDR);
 }
 
-void getMessageByConsole(Buffer* buf) {
+char* getDataByConsole() {
     char simbol;
-    while( (simbol=getchar()) != '\n' ) {
-        if (buf->current_size + 1 >= buf->max_size) {
-            buf->max_size *= 2;
-            buf->message = (char*)realloc(buf->message, buf->max_size);
+    int current_size = 0;
+    int max_size = START_SIZE_BUFFER;
+    char* message = (char*) calloc(max_size + 1, sizeof(char));
+
+    printf("data = ");
+    while( (simbol=getchar()) != '\n') {
+        if (current_size > max_size) {
+            max_size *= 2;
+            message = (char*)realloc(message, max_size);
         }
-        buf->message[buf->current_size] = simbol;
-        buf->current_size += 1;
+        message[current_size] = simbol;
+        current_size += 1;
     }
-    buf->message[buf->current_size] = '\0';
+    message[current_size] = '\0';
+    return message;
 }
 
-char* createPacageMessage(char* message) {
-    int size_pacage_message = strlen(START_HEAD) + strlen(START_JSON) +
-                              strlen(message)    + strlen(END_JSON)   + 1;
-    char* pacage_message = (char*)calloc(size_pacage_message, sizeof(char));
+char* pacageMessage(char* data) {
     int index = 0;
+    int size_pacage_message = strlen(START_HEAD) + strlen(START_JSON) +
+                              strlen(data)       + strlen(END_JSON)   + 1;
+    char* pacage_message = (char*)calloc(size_pacage_message, sizeof(char));
+
     memcpy(pacage_message + index, START_HEAD, strlen(START_HEAD));
     index += strlen(START_HEAD);
     memcpy(pacage_message + index, START_JSON, strlen(START_JSON));
     index += strlen(START_JSON);
-    memcpy(pacage_message + index, message   , strlen(message));
-    index +=  strlen(message);
-    memcpy(pacage_message + index, END_JSON  , strlen(END_JSON));
+    memcpy(pacage_message + index, data,       strlen(data));
+    index +=  strlen(data);
+    memcpy(pacage_message + index, END_JSON,   strlen(END_JSON));
     index += strlen(END_JSON);
     pacage_message[index] = '\0';
     return pacage_message;
@@ -88,27 +126,29 @@ char* createPacageMessage(char* message) {
 
 int main(int argc, char const* argv[])
 {
-    int valread;
     struct sockaddr_in serv_addr;
-    Buffer buf;
-    buf.current_size = 0;                        
-    buf.max_size = START_SIZE_BUFFER;
-    buf.message = (char*) calloc(buf.max_size, sizeof(char));
 
     int client_fd = createSoket();
-    setAddresSetting(&serv_addr);
-    getMessageByConsole(&buf);
+    setAddresSetting(&serv_addr);    
     tryConnect(client_fd, &serv_addr);
 
-    char* pacage_message = createPacageMessage(buf.message);
-    printf("%s\n", pacage_message);
-    send(client_fd, pacage_message, strlen(pacage_message), 0);
-    printf("Hello message sent\n");
-    valread = read(client_fd, buf.message, buf.max_size);
-    buf.message[valread] = '\0';
-    printf("(start)%s(end)\n", buf.message);
+    char* data = getDataByConsole();
+   // printf("%s\n", data);
+
+    char* request_message = pacageMessage(data);
+   // printf("%s\n", request_message);
+    send(client_fd, request_message, strlen(request_message), 0);
+
+
+    char* response_message = readMessageBySoket(client_fd);
+    //printf("%s\n", response_message);
+
+    
+    hanleRespone(response_message);
+
     close(client_fd);
-    free(buf.message);
-    free(pacage_message);
+    free(data);
+    free(request_message);
+    free(response_message);
     return 0;
 }
